9faeafe1df20bc41d2c02d48b20ad6844a585e60
maeva.poirier2022@campus-eni.fr <Axel15042020>
2022-11-30 12:28:14 +0100

    bug home.js

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -1,57 +1,52 @@
-const { async } = require("regenerator-runtime");
+// async function getData(url, token) {
+//     const options = {
+//         method: 'GET',
+//         headers: {
+//             'accept': 'application/json',
+//             'authorization' : 'Bearer ' + token
+//         }
+//     }
 
+//     try {
+//         const response = await fetch(url, options);
 
-async function getData(url, token) {
-
-    const options = {
-        method: 'GET',
-        headers: {
-            'accept': 'application/json',
-            'authorization' : 'Bearer ' + token
-        }
-    }
-
-    try {
-        const response = await fetch(url, options);
-
-        if (response.ok) {
-            const data = await response.json();
-            return data;
-        } else {
-            console.error(`Erreur de réponse ${error}`);
-        }
+//         if (response.ok) {
+//             const data = await response.json();
+//             return data;
+//         } else {
+//             console.error(`Erreur de réponse ${error}`);
+//         }
         
-    } catch (error) {
-        console.error(`Erreur de fetch ${error}`);
-    } 
+//     } catch (error) {
+//         console.error(`Erreur de fetch ${error}`);
+//     } 
 
-}
+// }
 
+// const showProjects = (data) => {
+//     for (let element of data) {
 
-const showProjects = (data) => {
-    for (let element of data) {
+//         console.log("fonction showProjects appelée");
+//         let myProject = document.createElement('tr');
+//         let myLogo = document.createElement('td');
+//         let myImg = document.createElement('img');
+//         let myName = document.createElement('td');
+//         let myStatus = document.createElement('td');
 
-        console.log("fonction showProjects appelée");
-        let myProject = document.createElement('tr');
-        let myLogo = document.createElement('td');
-        let myImg = document.createElement('img');
-        let myName = document.createElement('td');
-        let myStatus = document.createElement('td');
+//         myImg.src = "build/images/mini-logoipsum.jpg";
+//         myImg.alt = "logo client";
+//         myName.textContent = element.name;
+//         myStatus.textContent = "Statut du projet";
 
-        myImg.src = "build/images/mini-logoipsum.jpg";
-        myImg.alt = "logo client";
-        myName.textContent = element.name;
-        myStatus.textContent = "Statut du projet";
+//         myLogo.appendChild(myImg);
+//         myProject.appendChild(myLogo);
+//         myProject.appendChild(myName);
+//         myProject.appendChild(myStatus);
 
-        myLogo.appendChild(myImg);
-        myProject.appendChild(myLogo);
-        myProject.appendChild(myName);
-        myProject.appendChild(myStatus);
-
-        let projectTable = document.getElementById("ProjectsTable");
-        projectTable.appendChild(myProject);
-    }
-}
+//         let projectTable = document.getElementById("ProjectsTable");
+//         projectTable.appendChild(myProject);
+//     }
+// }
 
 const getProjects = async (apiUrl, clientId, token) => {
     console.log('fonction getProjects appelée');
